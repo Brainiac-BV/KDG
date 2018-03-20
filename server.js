bf58a7092fa7aba4a3abd31326bddb22653700a0
unknown <k.dgray23@gmail.com>
2018-03-20 13:38:21 -0700

    fixed package.json

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const path = require('path')
 const express = require('express')
-const layout = require('express-layout')
 const routes = require('./routes')
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
 const middleware = [
   layout(),
